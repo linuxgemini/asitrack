727df7ea482c81c5ad4f087286ea85406aa459fb
İlteriş Yağıztegin Eroğlu <ilteris@asenkron.com.tr>
2021-02-15 02:35:56 +0300

    eslint pass, better silent fail

--- asi.js
+++ asi.js
@@ -3,75 +3,85 @@
 const cheerio		=	require("cheerio");
 const dayjs		=	require("dayjs");
 const fetch		=	require("node-fetch");
-const sleep		=	require("util").promisify(setTimeout);
-const utc		=	require('dayjs/plugin/utc');
+const utc		=	require("dayjs/plugin/utc");
 const varRegExp		=	/^var [\w\d]+ = [\w\d:'"]+;?$/i;
 
 require("dayjs/locale/tr");
 dayjs.locale("en");
-dayjs.extend(utc)
+dayjs.extend(utc);
 
 const strProcess = (str) => str.replace(/^(\s+)|(\s+)$/g, "").replace(/;+$/, "").toLowerCase();
 const removeQuote = (str) => str.replace(/^("|')|("|')$/g, "");
 
-const getRaw = async _ => {
-	let vals = {};
+const getRaw = async () => {
+    let vals = {};
 
-	try {
-		const page = await fetch("https://covid19asi.saglik.gov.tr/");
-		if (!page.ok) throw new Error("boink");
+    try {
+        const page = await fetch("https://covid19asi.saglik.gov.tr/");
+        if (!page.ok) throw new Error("boink");
 
-		const txt = await page.text();
-		const $ = cheerio.load(txt);
+        const txt = await page.text();
+        const $ = cheerio.load(txt);
 
 
-		const data = $("script");
-		for (const init of data) {
-			for (const children of init.children) {
-				const intxt = strProcess(children.data);
-				if (!varRegExp.test(intxt)) continue;
-	
-				const parsingArr = intxt.split(/\s+/);
-				const obj = JSON.parse(`{"${parsingArr[1]}": "${removeQuote(parsingArr[3])}"}`);
-				vals = {...vals, ...obj};
-			}
-		}
-	} catch (e) {
-		throw e;
-	} finally {
-		return vals;
-	}
+        const data = $("script");
+        for (const init of data) {
+            for (const children of init.children) {
+                const intxt = strProcess(children.data);
+                if (!varRegExp.test(intxt)) continue;
+    
+                const parsingArr = intxt.split(/\s+/);
+                const obj = JSON.parse(`{"${parsingArr[1]}": "${removeQuote(parsingArr[3])}"}`);
+                vals = {...vals, ...obj};
+            }
+        }
+    } catch (e) {
+        return vals;
+    }
 };
 
-const main = async _ => {
-	const now =  dayjs().utcOffset(3);
+const main = async () => {
+    const now =  dayjs().utcOffset(3);
 
-	const vals = await getRaw();
+    const vals = await getRaw();
 
-	const hour = (vals.asisayisiguncellemesaati ? parseInt(vals.asisayisiguncellemesaati.split(":")[0], 10) : now.hour());
-	const minute = (vals.asisayisiguncellemesaati ? parseInt(vals.asisayisiguncellemesaati.split(":")[1], 10) : now.minute());
+    const timeExists = (vals.asisayisiguncellemesaati ? true : false);
+    const vaxCountExists = (vals.asiyapilankisisayisi ? true : false);
+    const firstDoseCountExists = (vals.asiyapilankisisayisi1doz ? true : false);
+    const secondDoseCountExists = (vals.asiyapilankisisayisi2doz ? true : false);
+    const totalDoseCountExists = (vals.yapilanasisayisi ? true : false);
+    const isFailed = (firstDoseCountExists && secondDoseCountExists);
 
-	const vaccinatedPeopleCount = (vals.asiyapilankisisayisi ? parseInt(vals.asiyapilankisisayisi, 10) : 0);
-	const firstDoseCount = (vals.asiyapilankisisayisi1doz ? parseInt(vals.asiyapilankisisayisi1doz, 10) : 0);
-	const secondDoseCount = (vals.asiyapilankisisayisi2doz ? parseInt(vals.asiyapilankisisayisi2doz, 10) : 0);
-	const totalCount = (vals.yapilanasisayisi ? parseInt(vals.yapilanasisayisi, 10) : 0);
-	const calculatedTotalCount = firstDoseCount + secondDoseCount;
-	const doesTotalCountMatchCalculation = totalCount === calculatedTotalCount;
-	const lastUpdated = now.hour(hour).minute(minute).second(0).millisecond(0).toDate();
+    const hour = (timeExists ? parseInt(vals.asisayisiguncellemesaati.split(":")[0], 10) : now.hour());
+    const minute = (timeExists ? parseInt(vals.asisayisiguncellemesaati.split(":")[1], 10) : now.minute());
 
-	const final = {
-		vaccinatedPeopleCount,
-		vaccinatedPeople: {
-			firstDoseCount,
-			secondDoseCount,
-			totalCount,
-			calculatedTotalCount,
-			doesTotalCountMatchCalculation
-		},
-		lastUpdated
-	};
+    // legacy
+    const vaccinatedPeopleCount = (vaxCountExists ? parseInt(vals.asiyapilankisisayisi, 10) : NaN);
 
-	return final;
+    // latest
+    const firstDoseCount = (firstDoseCountExists ? parseInt(vals.asiyapilankisisayisi1doz, 10) : NaN);
+    const secondDoseCount = (secondDoseCountExists ? parseInt(vals.asiyapilankisisayisi2doz, 10) : NaN);
+    const totalCount = (totalDoseCountExists ? parseInt(vals.yapilanasisayisi, 10) : NaN);
+
+    const calculatedTotalCount = firstDoseCount + secondDoseCount;
+    const doesTotalCountMatchCalculation = totalCount === calculatedTotalCount;
+
+    const lastUpdated = now.hour(hour).minute(minute).second(0).millisecond(0).toDate();
+
+    const final = {
+        isFailed,
+        vaccinatedPeopleCount,
+        vaccinatedPeople: {
+            firstDoseCount,
+            secondDoseCount,
+            totalCount,
+            calculatedTotalCount,
+            doesTotalCountMatchCalculation
+        },
+        lastUpdated
+    };
+
+    return final;
 };
 
 module.exports = main;
--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const fs	=	require("fs");
 const getAsi	=	require("./asi");
 
 const main = async () => {
-	const now =  dayjs().utcOffset(3);
-	const data = await getAsi();
-	fs.writeFileSync("./asibilgisi.json", JSON.stringify(data, null, 4));
-	console.log(now.unix());
-	return null;
+    const now =  dayjs().utcOffset(3);
+    const data = await getAsi();
+
+    if (data.isFailed) return;
+    delete data.isFailed;
+
+    fs.writeFileSync("./asibilgisi.json", JSON.stringify(data, null, 4));
+    console.log(now.unix());
+    return null;
 };
 
 setInterval(main, (30 * 1000)); // execute almost every half minute
